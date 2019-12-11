fa9f81ad6337a267cf96ecdb82cd8ff6854ac332
Spacehuhn <github@spacehuhn.com>
2019-12-11 17:53:06 +0100

    Removed unused EEPROM feature

--- nanobill/eeprom.cpp
+++ nanobill/eeprom.cpp
@@ -8,26 +8,10 @@
 
 #include "config.h"
 
-// Used to verify memory
-typedef struct boot_t {
-    unsigned int magic_num : 32;
-    unsigned int boot_num  : 8;
-} boot_t;
 
 namespace eeprom {
     void begin() {
         EEPROM.begin();
-
-        boot_t b;
-
-        EEPROM.get(EEPROM_BOOT_ADDR, b);
-
-        if ((b.magic_num == BOOT_MAGIC_NUM) && (b.boot_num < 3)) {
-            save(EEPROM_BOOT_ADDR, boot_t { BOOT_MAGIC_NUM, ++b.boot_num });
-            return;
-        } else {
-            save(EEPROM_BOOT_ADDR, boot_t { BOOT_MAGIC_NUM, 1 });
-        }
     }
 
     void end() {
